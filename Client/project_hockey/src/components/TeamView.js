d70faa1cfc0deb1963d24057a5963b5b1db195a8
Ossi <karja.ossi@gmail.com>
2019-12-19 11:34:10 +0200

    Ayee lmao

--- Client/project_hockey/src/components/TeamView.js
+++ Client/project_hockey/src/components/TeamView.js
@@ -6,6 +6,8 @@ import APIconnection from '../APIconnection';
 import team1 from './team-images/team1.png';
 import team2 from './team-images/team2.png';
 import team3 from './team-images/team3.png';
+import team4 from './team-images/team4.png';
+import team5 from './team-images/team5.png';
 
 export default function TeamView(props) {
     let ParsedUser = JSON.parse(sessionStorage.getItem("User"));
@@ -112,9 +114,26 @@ export default function TeamView(props) {
         }
     }
     function teamimage(){
-        if(Team.teamid === 1)
+        var teamid = parseInt(props.match.params.id);
+        if(teamid === 1)
         {
-           return (<p>test</p>)
+           return (<img style={{height: "500px", width:"540px", overflow:"visible"}} src={team1} alt="No team image available"></img>)
+        }
+        if(teamid === 2)
+        {
+           return (<img style={{height: "500px", width:"540px", overflow:"visible"}} src={team2} alt="No team image available"></img>)
+        }
+        if(teamid === 3)
+        {
+           return (<img style={{height: "500px", width:"540px", overflow:"visible"}} src={team3} alt="No team image available"></img>)
+        }
+        if(teamid === 4)
+        {
+           return (<img style={{height: "500px", width:"500px", overflow:"visible"}} src={team4} alt="No team image available"></img>)
+        }
+        if(teamid === 5)
+        {
+           return (<img style={{height: "500px", width:"500px", overflow:"visible"}} src={team5} alt="No team image available"></img>)
         }
     }
 
@@ -127,16 +146,17 @@ export default function TeamView(props) {
 
             <div>
                 <div>
-                    <div>
+                    <div style={{width: "50%", display: "flex",  float:"right", justifyContent:"center"}}>
                         {teamimage()}
                     </div>
-                    {Team.map(team => <div><h1>{team.teamname}</h1> <br/> <h3 style={{marginLeft: "auto", marginRight: "auto", width: "40%"}}>{team.teaminfo}</h3></div>)} 
-                    The owner of this team is <b><Link to={ `/users/${OwnerId}` }>{TeamOwner}</Link></b>
-                    <h3>Stats:</h3>
+                    <div style={{width: "50%", marginTop: "50px", float:"left"}}>
+                    {Team.map(team => <div><u><h1>{team.teamname}</h1></u> <h3 style={{marginLeft: "auto", marginRight: "auto", width: "40%"}}>{team.teaminfo}</h3></div>)} 
+                    Teamowner: <b><Link to={ `/users/${OwnerId}` }>{TeamOwner}</Link></b>
                     {Team.map(team => <div><a style={{ color: "green", fontSize: "25px" }}>{team.teamwins} wins</a> and <a style={{ color: "red", fontSize: "25px"  }}> {team.teamlosses} losses</a></div>)} 
-                   <br></br> <p style={{ fontSize: "25px" }}>Members:</p> {TeamMembers.map(member => <div><Link to={ `/users/${member.id}` }>{member.username}</Link></div>)}
+                     <p style={{ fontSize: "25px", marginTop:"10px", marginBottom:"10px" }}>Members:</p> {TeamMembers.map(member => <div><Link to={ `/users/${member.id}` }>{member.username}</Link></div>)}
                     <br></br>
                     {noteam()}
+                    </div>
                     
                 <div>
             </div>
