bffd67829e8c118c5143bc20411842707a6f1946
Oscar Blide <oscarblide@gmail.com>
2019-12-19 08:26:56 +0100

    Added background

--- Client/project_hockey/src/components/NotLoggedinHeader.js
+++ Client/project_hockey/src/components/NotLoggedinHeader.js
@@ -5,6 +5,7 @@ import APIconnection from '../APIconnection.json';
 import axios from 'axios';
 import SignUpForm from './SignUpForm';
 import hockeylogo from '../hockeylogo.svg';
+import hockeybg from '../hockeybg.svg';
 
 
 export default function Header(props) {
@@ -96,6 +97,11 @@ export default function Header(props) {
       <div id="signup-div">
         <SignUpForm setUserInfo={props.setUserInfo} UserInfo={props.UserInfo} loginSuccess={props.loginSuccess} redirectPathOnSuccess={props.redirectPathOnSuccess} />
         <button className="existing-button" type="submit" onClick={changeSignup2}>Already have an account?</button>
+      </div>
+      <div>
+        <div className="hockeybackground">
+          <img className="hockeybg" src={hockeybg} />
+        </div>
       </div>
     </div>
   )
